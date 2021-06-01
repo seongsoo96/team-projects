d9d30937d16ac1ef53f8c108b495a544ef5e2f25
HwangJunHa <tarot1415@gmail.com>
2021-06-01 12:39:28 +0900

    소셜 로그인, 사이트 회원가입, 아이디 찾기 완료

--- src/main/java/ppeonfun/controller/user/member/MemberController.java
+++ src/main/java/ppeonfun/controller/user/member/MemberController.java
@@ -1,14 +1,23 @@
 package ppeonfun.controller.user.member;
 
 
+import java.util.Date;
+import java.util.HashMap;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ppeonfun.dto.Member;
 import ppeonfun.service.user.member.MemberService;
@@ -20,9 +29,79 @@ public class MemberController {
 	
 	@Autowired private MemberService memberService;
 	
+	
 	@RequestMapping(value="/loginForm", method=RequestMethod.GET)
 	public void loginForm() {
 		logger.info("/login 실행");
+	}
+	
+	@RequestMapping(value="/joinSelect", method=RequestMethod.GET)
+	public void joinSelect() {
+		logger.info("/joinSelect 실행");
+	}
+	
+	@RequestMapping(value="/kakao/loginForm")
+	public String kakaLogin(HttpSession session) {
+		String url= "https://kauth.kakao.com/oauth/authorize?client_id=684fd291495baa02477d2ce31d0e378b&redirect_uri=http://localhost:8088/user/member/kakao/login&response_type=code";
+		session.removeAttribute("join");
+		
+		return "redirect:"+url;
+	}
+	@RequestMapping(value="/idFind" , method=RequestMethod.GET)
+	public void idFind() {
+		logger.info("/idFind [GET]실행");
+	}
+	@RequestMapping(value="/idFind" , method=RequestMethod.POST)
+	public String idFindSuccess(String mEmail, Model model) {
+		logger.info("/idFind [POST]실행");
+		logger.info("email {}", mEmail);
+		String id = memberService.idFind(mEmail);
+		
+		model.addAttribute("id",id);
+		return "/user/member/idFindSuccess";
+	}
+	
+	@RequestMapping(value="/kakao/login")
+	public String kakaoLogin(String code,HttpSession session) {
+		logger.info("/kakao/login code: {}", code);
+		boolean isJoin= false;
+		
+		String accessToken=memberService.kakaoGetAccessToken(code);
+		logger.info("accessToken : {}", accessToken);
+		HashMap<String, Object> userInfo = memberService.getUserInfo(accessToken);
+	    for(String e : userInfo.keySet()) {
+	    	logger.info("{} : {}",e, userInfo.get(e));
+	    }
+	    //값이 존재 했을때
+	    if(userInfo!=null && !"".equals((String)userInfo.get("email"))) {
+	    	isJoin = memberService.kakaoIdCheck((String)userInfo.get("email"));
+	    }
+	    //true 아이디 존재
+	    if(isJoin) {
+	    	int mNo = memberService.kakaoMno((String)userInfo.get("email"));
+	    	session.setAttribute("mNo", mNo);
+	    	session.setAttribute("accessToken", accessToken);
+	    }else { // 아이디가 존재 하지 않음 자동 회원가입
+	    	Member member=memberService.joinKakao(userInfo);
+	    	if(member.getmNo() > 0) {
+				session.setAttribute("mNo", member.getmNo());
+				session.setAttribute("accessToken", accessToken);
+			}
+	    }
+	    
+	    return "redirect:/";
+		
+	}
+	@RequestMapping(value="/kakao/logout")
+	public String kakaoLogout(HttpSession session, HttpServletRequest request, HttpServletResponse response) {
+		memberService.kakaoLogout((String)session.getAttribute("accessToken"));
+		session.invalidate();
+//		Cookie[] cookies = request.getCookies();
+//		for(int i=0; i<cookies.length; i++) {
+//			logger.info("이름:{}, 값:{}", cookies[i].getName(), cookies[i].getValue());
+//		}
+		
+		return "redirect:/";
 	}
 	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
@@ -36,7 +115,7 @@ public class MemberController {
 			session.setAttribute("mNick", member.getmNick());
 		}
 		
-		if(member.getmGrade()!=null&&"M".equals(member.getmGrade())) {
+		if(member!=null&&"M".equals(member.getmGrade())) {
 			return "redirect:/admin/main";
 		}
 			
@@ -54,7 +133,61 @@ public class MemberController {
 	@RequestMapping(value = "/joinForm", method = RequestMethod.GET)
 	public void joinForm() {
 		logger.info("/joinForm [GET] 실행");
-		
-		
 	}
+	
+	@RequestMapping(value="/join")
+	public String join(Member member, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date birth, HttpSession session) {
+		logger.info("birth {}", birth);
+		logger.info("member {}", member);
+		member.setmBirth(birth); //생년월일 삽입
+		member = memberService.encryption(member); //비밀번호 암호화
+		
+		Member result = memberService.join(member);
+		logger.info("result: {}", result);
+		
+		if(result.getmNo() > 0) {
+			session.setAttribute("mNo", result.getmNo());
+			session.setAttribute("mNick", result.getmNick());
+		}
+		
+		
+		return "redirect:/";
+	}
+	
+	
+	
+	@RequestMapping(value="/idCheck", method=RequestMethod.POST)
+	public String idCheck(Member member, Model model) {
+		logger.info("/idCheck [POST]");
+		logger.info("Member {}", member);
+		boolean isId=memberService.idCheck(member);
+		
+		logger.info("isId: {}", isId);
+		model.addAttribute("isId", isId);
+		return "jsonView";
+	}
+	
+	@RequestMapping(value="/nickCheck", method=RequestMethod.POST)
+	public String nickCheck(Member member, Model model) {
+		logger.info("/nickCheck [POST]");
+		logger.info("Member {}", member);
+		boolean isNick=memberService.nickCheck(member);
+		
+		logger.info("isNick: {}", isNick);
+		model.addAttribute("isNick", isNick);
+		return "jsonView";
+	}
+	
+	
+	@RequestMapping(value="/email", method= RequestMethod.POST)
+	public String sendEmail(String email, Model model) {
+		logger.info("이메일 클릭 {}", email);
+		String authKey = memberService.sendAuthMail(email);
+        
+		model.addAttribute("authKey", authKey);
+		return "jsonView";
+	}
+	
+	
+	
 }
--- src/main/java/ppeonfun/service/user/member/MemberService.java
+++ src/main/java/ppeonfun/service/user/member/MemberService.java
@@ -1,5 +1,7 @@
 package ppeonfun.service.user.member;
 
+import java.util.HashMap;
+
 import ppeonfun.dto.Member;
 
 public interface MemberService {
@@ -19,4 +21,87 @@ public interface MemberService {
 	
 	public Member login(Member member);
 	
+	
+	/**
+	 * 메일 발송
+	 * @param email - 전송할 메일 주소
+	 * @return - 인증키
+	 */
+	public String sendAuthMail(String email);
+	/**
+	 * 아이디 체크
+	 * @param member - 아이디 
+	 * @return - 아이디가 있는지 없는지 체킹
+	 */
+	public boolean idCheck(Member member);
+	/**
+	 * 닉네임 체크
+	 * @param member - 닉네임
+	 * @return - 닉네임 중복 확인
+	 */
+	public boolean nickCheck(Member member);
+	
+	/**
+	 * 회원가입
+	 * @param member - 회원정보
+	 * @return - 회원정보
+	 */
+	public Member join(Member member);
+	
+	/**
+	 * 카카오 접근 토큰 받아오기
+	 * @param authoriz_code - 코드
+	 * @return - 토큰 반환
+	 */
+	public String kakaoGetAccessToken(String authoriz_code);
+	
+	/**
+	 * 카카오 유저 정보 가져오기
+	 * @param accessToken - 접근 토큰
+	 * @return - 유저 정보
+	 */
+	public HashMap<String, Object> getUserInfo(String accessToken);
+	/**
+	 * 카카오 유저 로그아웃
+	 * @param accessToken - 접근 토큰 
+	 */
+	public void kakaoLogout(String accessToken);
+	/**
+	 * 카카오 인증 토큰 얻어오기
+	 * @return 인증 토큰 얻어오기
+	 */
+	public String kakaoGetAuthorize();
+	/**
+	 * 카카오 계정 링크 끊기
+	 * @param accessToken - 유효한 접속 토큰
+	 */
+	public void kakaoUnlink(String accessToken);
+	
+	/**
+	 * 사이트 회원가입
+	 * @param userInfo - 사이트 
+	 * @return 회원가입 정보 전달
+	 */
+	
+	public Member joinKakao(HashMap<String, Object> userInfo);
+	/**
+	 * 카카오 아이디 체크
+	 * @param String - 이메일 
+	 * @return - 존재여부 반환
+	 */
+	public boolean kakaoIdCheck(String email);
+	
+	/**
+	 * 카카오로 회원가입된 회원번호 반환
+	 * @param email - 이메일로 검색
+	 * @return - mNo값
+	 */
+	public int kakaoMno(String email);
+	/**
+	 * 아이디 찾기
+	 * @param mEmail - 이메일로 검색
+	 * @return - 아이디 값
+	 */
+	public String idFind(String mEmail);
+
 }
